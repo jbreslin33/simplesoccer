21c1540424f1ca4ff0feadf11af260de3a257d78
Jim Breslin <jbreslin33@gmail.com>
2020-04-24 21:24:03 -0400

    support attacker

--- FieldPlayerStates.cpp
+++ FieldPlayerStates.cpp
@@ -234,68 +234,75 @@ SupportAttacker* SupportAttacker::Instance()
 
 void SupportAttacker::Enter(FieldPlayer* player)
 {
-  player->Steering()->ArriveOn();
+	if (player->mEnterLogs)
+	{
+		printf("SupportAttacker::Enter() ID:%d\n", player->ID());
+	}
+  	player->Steering()->ArriveOn();
 
-  player->Steering()->SetTarget(player->Team()->GetSupportSpot());
-  
-  #ifdef PLAYER_STATE_INFO_ON
-  //debug_con << "Player " << player->ID() << " enters support state" << "";
-  #endif
+  	player->Steering()->SetTarget(player->Team()->GetSupportSpot());
 }
 
 void SupportAttacker::Execute(FieldPlayer* player)                                     
 {
-  //if his team loses control go back home
-  if (!player->Team()->InControl())
-  {
-    player->GetFSM()->ChangeState(ReturnToHomeRegion::Instance()); return;
-  } 
+	if (player->mExecuteLogs)
+	{
+		printf("SupportAttacker::Execute() ID:%d\n", player->ID());
+	}
 
+  	//if his team loses control go back home
+  	if (!player->Team()->InControl())
+  	{
+    		player->GetFSM()->ChangeState(ReturnToHomeRegion::Instance()); return;
+  	}	 
 
-  //if the best supporting spot changes, change the steering target
-  if (player->Team()->GetSupportSpot() != player->Steering()->Target())
-  {    
-    player->Steering()->SetTarget(player->Team()->GetSupportSpot());
+  	//if the best supporting spot changes, change the steering target
+  	if (player->Team()->GetSupportSpot() != player->Steering()->Target())
+  	{    
+    		player->Steering()->SetTarget(player->Team()->GetSupportSpot());
+    		player->Steering()->ArriveOn();
+  	}
 
-    player->Steering()->ArriveOn();
-  }
-
-  //if this player has a shot at the goal AND the attacker can pass
-  //the ball to him the attacker should pass the ball to this player
-  if( player->Team()->CanShoot(player->Pos(),
+  	//if this player has a shot at the goal AND the attacker can pass
+  	//the ball to him the attacker should pass the ball to this player
+  	if( player->Team()->CanShoot(player->Pos(),
                                player->Pitch()->MaxShootingForce))
-  {
-    player->Team()->RequestPass(player);
-  }
+  	{
+    		player->Team()->RequestPass(player);
+  	}
 
-
-  //if this player is located at the support spot and his team still have
-  //possession, he should remain still and turn to face the ball
-  if (player->AtTarget())
-  {
-    player->Steering()->ArriveOff();
+  	//if this player is located at the support spot and his team still have
+  	//possession, he should remain still and turn to face the ball
+  	if (player->AtTarget())
+  	{
+    		player->Steering()->ArriveOff();
         
-    //the player should keep his eyes on the ball!
-    player->TrackBall();
+    		//the player should keep his eyes on the ball!
+    		player->TrackBall();
 
-    player->SetVelocity(Vector2D(0,0));
+    		player->SetVelocity(Vector2D(0,0));
 
-    //if not threatened by another player request a pass
-    if (!player->isThreatened())
-    {
-      player->Team()->RequestPass(player);
-    }
-  }
+    		//if not threatened by another player request a pass
+    		if (!player->isThreatened())
+    		{
+      			player->Team()->RequestPass(player);
+    		}
+  	}
 }
 
 
 void SupportAttacker::Exit(FieldPlayer* player)
 {
-  //set supporting player to null so that the team knows it has to 
-  //determine a new one.
-  player->Team()->SetSupportingPlayer(NULL);
+	if (player->mExitLogs)
+	{
+		printf("SupportAttacker::Exit() ID:%d\n", player->ID());
+	}
+  
+	//set supporting player to null so that the team knows it has to 
+  	//determine a new one.
+  	player->Team()->SetSupportingPlayer(NULL);
 
-  player->Steering()->ArriveOff();
+  	player->Steering()->ArriveOff();
 }
 
 
