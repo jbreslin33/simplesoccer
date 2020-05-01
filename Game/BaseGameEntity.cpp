14b82cb0feca9c1b92becc52e425dbd422801f87
Jim Breslin <jbreslin33@gmail.com>
2020-04-30 23:28:32 -0400

    moved vars BaseGame

--- Game/BaseGameEntity.cpp
+++ Game/BaseGameEntity.cpp
@@ -5,16 +5,19 @@ int BaseGameEntity::m_iNextValidID = 0;
 
 //------------------------------ ctor -----------------------------------------
 //-----------------------------------------------------------------------------
-BaseGameEntity::BaseGameEntity(int ID):m_dBoundingRadius(0.0),
-                                       m_vScale(Vector2D(1.0,1.0)),
-                                       m_iType(default_entity_type),
-                                       m_bTag(false)
+BaseGameEntity::BaseGameEntity(int ID)
 {
 	SetID(ID);
 
   	mEnterLogs = false;
   	mExecuteLogs = false;
   	mExitLogs = false;
+
+        m_dBoundingRadius = 0.0;
+        m_vScale = Vector2D(1.0,1.0);
+        m_iType = default_entity_type;
+        m_bTag = false;
+
 }
 
 //----------------------------- SetID -----------------------------------------
