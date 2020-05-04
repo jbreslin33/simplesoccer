66be7c195453af5d8b690454c57debf80f388f34
Jim Breslin <jbreslin33@gmail.com>
2020-05-03 20:15:54 -0400

    game

---
+++ game.cpp
@@ -0,0 +1,6 @@
+#include "game.h"
+
+Game::Game(int screenX, int screenY, Server* server, int id)
+{
+
+}
---
+++ game.h
@@ -0,0 +1,12 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Server;
+
+class Game
+{
+	public: 
+                Game(int screenX, int screenY, Server* server, int id);
+
+};
+#endif
