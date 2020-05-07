ad4a52150f450de0870b26b385e9c502e0d38abf
Jim Breslin <jbreslin33@gmail.com>
2020-05-07 17:24:15 -0400

    region

--- Game/Region.cpp
+++ Game/Region.cpp
@@ -1,1 +1,36 @@
 #include "Game/Region.h"
+
+Region::Region()
+{
+	mTop = 0;
+	mRight = 0;
+	mBottom = 0;
+}	
+
+Region::Region(double left, double top, double right, double bottom)
+{
+
+	mTop = top;
+	mRight = right;
+	mBottom = bottom;
+	//mId = id;
+
+	//calculate center of region
+        mCenter = Vector2D( (left+right)*0.5, (top+bottom)*0.5 );
+        mWidth  = fabs(right-left);
+        mHeight = fabs(bottom-top);
+}
+
+Region::Region(double left, double top, double right, double bottom, int id)
+{
+
+	mTop = top;
+	mRight = right;
+	mBottom = bottom;
+	mId = id;
+
+	//calculate center of region
+        mCenter = Vector2D( (left+right)*0.5, (top+bottom)*0.5 );
+        mWidth  = fabs(right-left);
+        mHeight = fabs(bottom-top);
+}
--- Game/Region.h
+++ Game/Region.h
@@ -26,48 +26,25 @@ class Region
   
 	protected:
 
-  		double        m_dTop;
-  		double        m_dLeft;
-  		double        m_dRight;
-  		double        m_dBottom;
+  		double        mTop;
+  		double        mLeft;
+  		double        mRight;
+  		double        mBottom;
 
-  		double        m_dWidth;
-  		double        m_dHeight;
+  		double        mWidth;
+  		double        mHeight;
 
-  		Vector2D     m_vCenter;
+  		Vector2D     mCenter;
   
-  		int          m_iID;
+  		int          mId;
 
 	public:
 
-  		Region():m_dTop(0),m_dBottom(0),m_dLeft(0),m_dRight(0)
-  		{
-  
-		}
 
-  		Region
-		(
-			double left,
-         		double top,
-         		double right,
-         		double bottom,
-         		int id = -1)
-			:
-				m_dTop(top),
-                        	m_dRight(right),
-                        	m_dLeft(left),
-                        	m_dBottom(bottom),
-                        	m_iID(id)
-  		{
-    
-			//calculate center of region
-    			m_vCenter = Vector2D( (left+right)*0.5, (top+bottom)*0.5 );
+  		Region();
+		Region(double left, double top, double right, double bottom);
+		Region(double left, double top, double right, double bottom, int id);
 
-    			m_dWidth  = fabs(right-left);
-    			m_dHeight = fabs(bottom-top);
-  		}
-
-  
 		virtual ~Region(){}
 
   		//returns true if the given position lays inside the region. The
@@ -79,45 +56,45 @@ class Region
   		inline Vector2D GetRandomPosition()const;
 
   		//-------------------------------
-  		double     Top()const{return m_dTop;}
-  		double     Bottom()const{return m_dBottom;}
+  		double     Top()const{return mTop;}
+  		double     Bottom()const{return mBottom;}
 		double     Left()const
   		{
-			return m_dLeft;
+			return mLeft;
 		}
   
-		double     Right()const{return m_dRight;}
-  		double     Width()const{return fabs(m_dRight - m_dLeft);}
-  		double     Height()const{return fabs(m_dTop - m_dBottom);}
+		double     Right()const{return mRight;}
+  		double     Width()const{return fabs(mRight - mLeft);}
+  		double     Height()const{return fabs(mTop - mBottom);}
   		double     Length()const{return std::max(Width(), Height());}
   		double     Breadth()const{return std::min(Width(), Height());}
 
-  		Vector2D  Center()const{return m_vCenter;}
-  		int       ID()const{return m_iID;}
+  		Vector2D  Center()const{return mCenter;}
+  		int       ID()const{return mId;}
 
 };
 
 
 inline Vector2D Region::GetRandomPosition()const
 {
-	return Vector2D(RandInRange(m_dLeft, m_dRight),
-                   RandInRange(m_dTop, m_dBottom));
+	return Vector2D(RandInRange(mLeft, mRight),
+                   RandInRange(mTop, mBottom));
 }
 
 inline bool Region::Inside(Vector2D pos, region_modifier r=normal)const
 {
 	if (r == normal)
   	{
-    		return ((pos.x > m_dLeft) && (pos.x < m_dRight) &&
-         	(pos.y > m_dTop) && (pos.y < m_dBottom));
+    		return ((pos.x > mLeft) && (pos.x < mRight) &&
+         	(pos.y > mTop) && (pos.y < mBottom));
   	}
   	else
   	{
-    		const double marginX = m_dWidth * 0.25;
-    		const double marginY = m_dHeight * 0.25;
+    		const double marginX = mWidth * 0.25;
+    		const double marginY = mHeight * 0.25;
 
-    		return ((pos.x > (m_dLeft+marginX)) && (pos.x < (m_dRight-marginX)) &&
-         		(pos.y > (m_dTop+marginY)) && (pos.y < (m_dBottom-marginY)));
+    		return ((pos.x > (mLeft+marginX)) && (pos.x < (mRight-marginX)) &&
+         		(pos.y > (mTop+marginY)) && (pos.y < (mBottom-marginY)));
   	}
 }
 
